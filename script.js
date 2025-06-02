06ebd9ba4df12b071191bbd8662bf50f96038f23
Josh <joshedralin@gmail.com>
2025-06-02 00:29:55 -0700

    Add index.html and script.js
    Set up intial index.html and added creation of 16x16 grid. The creation
    of the grid is static, will probably end up changing it.

---
+++ script.js
@@ -0,0 +1,17 @@
+// Create 16x16 grid of square divs into container
+for (let i = 0; i < 16; i++) {
+    const divContainer = document.createElement('div');
+    divContainer.style.display = 'flex';
+
+    const container = document.querySelector('.container');
+    container.appendChild(divContainer);
+
+    for (let j = 0; j < 16; j++) {
+        const div = document.createElement('div');
+        div.style.width = "50px";
+        div.style.height = "50px";
+        div.style.border = '1px solid black';
+        
+        divContainer.appendChild(div);
+    }
+}
